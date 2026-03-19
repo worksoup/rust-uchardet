bd3c60ef1407ca70f6fed5132a0334ff74c9107b
汤意工坊 <78455197+worksoup@users.noreply.github.com>
2026-03-19 12:36:41 +0800

    patch CMakeLists.txt to remove "-fsanitize=address".

--- uchardet-git-sys/build.rs
+++ uchardet-git-sys/build.rs
@@ -21,9 +21,13 @@
 // SOFTWARE.
 
 use cmake::Config;
-use std::{env, path::PathBuf};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     if let Ok(lib) = pkg_config::Config::new().probe("uchardet")
         // 目前与已发布版本不兼容。
         && false
@@ -34,44 +38,16 @@ fn main() {
             .map(|p| format!("-I{}", p.display()))
             .collect();
         let header = find_header(&lib.include_paths).unwrap_or_else(|| PathBuf::from("uchardet.h"));
-        generate_bindings(header, include_args);
+        generate_bindings(out_dir, header, include_args);
     } else {
         let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-        let cpp_stdlib = 'cpp_stdlib: {
-            Some(match target_os.as_str() {
-                "macos" => "c++",
-                "windows" => {
-                    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
-                    if target_env.as_str() == "gnu" {
-                        "stdc++"
-                    } else {
-                        break 'cpp_stdlib None;
-                    }
-                }
-                _ => "stdc++",
-            })
-        };
-        let profile = env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
-        let cmake_build_type = if profile == "release" {
-            Some("Release")
-        } else if target_os.as_str() != "windows" {
-            Some("RelWithDebInfo")
-        } else {
-            None
-        };
-        if let Some(cpp_stdlib) = cpp_stdlib {
-            println!("cargo:rustc-link-lib={}", cpp_stdlib)
-        }
-        eprintln!("pkg-config failed, building uchardet from source");
-        let mut config = Config::new("uchardet");
-        config
+        select_cxx_stdlib(target_os);
+        let patched_src = patch_src(&out_dir);
+        let dst = Config::new(patched_src)
             .define("BUILD_BINARY", "OFF")
             .define("BUILD_STATIC", "ON")
-            .define("BUILD_SHARED_LIBS", "OFF");
-        if let Some(cmake_build_type) = cmake_build_type {
-            config.define("CMAKE_BUILD_TYPE", cmake_build_type);
-        }
-        let dst = config.build();
+            .define("BUILD_SHARED_LIBS", "OFF")
+            .build();
         // 输出链接指令
         println!("cargo:rustc-link-search=native={}/lib", dst.display());
         println!("cargo:rustc-link-search=native={}/lib64", dst.display());
@@ -79,7 +55,56 @@ fn main() {
 
         let header = PathBuf::from("uchardet/src/uchardet.h");
         let include_args = vec!["-Iuchardet/src".to_string()];
-        generate_bindings(header, include_args);
+        generate_bindings(out_dir, header, include_args);
+    }
+}
+
+fn patch_src(out_dir: impl AsRef<Path>) -> PathBuf {
+    let original_src = Path::new("uchardet");
+    if !original_src.exists() {
+        panic!(
+            "Original source directory '{}' not found.",
+            original_src.display()
+        );
+    }
+    if cfg!(feature = "source_no_patch") {
+        return original_src.to_path_buf();
+    }
+    let patched_src = out_dir.as_ref().join("uchardet_patched");
+    if patched_src.exists() {
+        // out_dir.
+        fs::remove_dir_all(&patched_src).expect("Failed to remove old patched source");
+    }
+    copy_dir_all(original_src, &patched_src).expect("Failed to copy source");
+    let cmakelists_path = patched_src.join("CMakeLists.txt");
+    let content = fs::read_to_string(&cmakelists_path).expect("Failed to read CMakeLists.txt");
+
+    let new_content: Vec<&str> = content
+        .lines()
+        .filter(|line| !line.contains("-fsanitize=address"))
+        .collect();
+
+    fs::write(&cmakelists_path, new_content.join("\n")).expect("Failed to write CMakeLists.txt");
+    patched_src
+}
+
+fn select_cxx_stdlib(target_os: String) {
+    let cpp_stdlib = 'cpp_stdlib: {
+        Some(match target_os.as_str() {
+            "macos" => "c++",
+            "windows" => {
+                let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
+                if target_env.as_str() == "gnu" {
+                    "stdc++"
+                } else {
+                    break 'cpp_stdlib None;
+                }
+            }
+            _ => "stdc++",
+        })
+    };
+    if let Some(cpp_stdlib) = cpp_stdlib {
+        println!("cargo:rustc-link-lib={}", cpp_stdlib)
     }
 }
 
@@ -97,7 +122,7 @@ fn find_header(include_paths: &[PathBuf]) -> Option<PathBuf> {
     None
 }
 
-fn generate_bindings(header: PathBuf, include_args: Vec<String>) {
+fn generate_bindings(out_path: impl AsRef<Path>, header: PathBuf, include_args: Vec<String>) {
     let bindings = bindgen::Builder::default()
         .header(header.to_str().expect("header path not valid UTF-8"))
         .use_core()
@@ -108,8 +133,23 @@ fn generate_bindings(header: PathBuf, include_args: Vec<String>) {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_path.as_ref().join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        let src_path = entry.path();
+        let dst_path = dst.as_ref().join(entry.file_name());
+        if ty.is_dir() {
+            copy_dir_all(&src_path, &dst_path)?;
+        } else {
+            fs::copy(&src_path, &dst_path)?;
+        }
+    }
+    Ok(())
 }
